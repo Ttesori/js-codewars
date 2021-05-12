fd42ce214bf8426f9e2072dc96eda7fdb158b6ce
Toni Tesori <tt@tesoridesign.com>
2021-05-12 14:03:17 -0400

    Add multiTapKeyboard

--- 2021-05/6-multiTapKeypad.js
+++ 2021-05/6-multiTapKeypad.js
@@ -2,7 +2,6 @@
 
 const presses = (phrase) => {
   let presses = 0;
-
   phrase.toUpperCase().split('').forEach(char => {
     presses += getKeyValue(char);
   });
@@ -10,10 +9,11 @@ const presses = (phrase) => {
 }
 
 const getKeyValue = (key) => {
-  let keys = ['ABC2', 'DEF3', 'GHI4', 'JKL5', 'MNO6', 'PQRS7', 'TUV8', 'WXYZ9', ' '];
+  let keys = ['1', 'ABC2', 'DEF3', 'GHI4', 'JKL5', 'MNO6', 'PQRS7', 'TUV8', 'WXYZ9', ' 0'];
   let answer = keys.filter(keyGroup => keyGroup.indexOf(key) !== -1);
   return answer[0].indexOf(key) + 1;
 }
-
 console.log(presses("LOL") === 9);
 console.log(presses("HOW R U") === 13);
+console.log(presses("HOW R U") === 13);
+console.log(presses("IS NE1 OUT THERE") === 31);
