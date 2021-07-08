dc4be27345b3196c5836b568a74d605423d76387
Toni Tesori <tt@tesoridesign.com>
2021-07-08 11:40:58 -0400

    Today's problem

---
+++ 2021-07/d-0708.js
@@ -0,0 +1,14 @@
+// Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+function solve(arr) {
+  // the answer is the number of unique elements in each array multiplied together
+  const setArr = arr.map(arr => Array.from(new Set(arr)));
+  return setArr.reduce((count, arr) => count * arr.length, 1);
+}
+
+console.log(solve([[1, 2], [4], [5, 6]]), 4); // 2 * 1 * 2 => 4
+console.log(solve([[1, 2], [4, 4], [5, 6, 6]]), 4); // 2 * 1 * 2 => 4
+console.log(solve([[1, 2], [3, 4], [5, 6]]), 8); // 2 * 2 * 2 => 8
+console.log(solve([[1, 2, 3], [3, 4, 6, 6, 7], [8, 9, 10, 12, 5, 6]]), 72); // 3 * 4 * 6 => 72
