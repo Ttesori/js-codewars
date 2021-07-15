dec8360c4d98eb11dd9ad016c280c99dafe9a6db
Toni Tesori <tt@tesoridesign.com>
2021-07-15 13:40:53 -0400

    Misunderstood the problem lol

--- 2021-07/d-0715.js
+++ 2021-07/d-0715.js
@@ -4,22 +4,28 @@
 //Note: You will have to handle multiple spaces, and leading/trailing spaces. You may assume the input only contain English alphabet and spaces.
 
 const transformString = (str) => {
-  // Trim string
-  str = str.trim();
-  // Deal with extra spaces - take down to one space
-  str = str.replace(/\s+/g, " ");
-  // Reverse the order of words & join
-  str = str.split(' ').reverse().join(' ');
-  // Change case of every character
-  newStr = '';
-  for (let i = 0; i < str.length; i++) {
+  let newStr = '';
+  let currWord = [];
+  for (let i = str.length - 1; i >= 0; i--) {
     let char = str[i];
-    newStr += char.charCodeAt(0) < 90 ? char.toLowerCase() : char.toUpperCase();
+    if (char === ' ' && currWord.length > 0) {
+      newStr += currWord.reverse().join('') + char;
+      currWord = [];
+    } else if (char === ' ') {
+      newStr += char;
+    } else {
+      char = char.charCodeAt(0) <= 90 ? char.toLowerCase() : char.toUpperCase();
+      currWord.push(char)
+    }
+  }
+  if (currWord.length > 0) {
+    newStr += currWord.reverse().join('');
   }
   return newStr;
 }
 
 console.log(transformString("Example Input") === "iNPUT eXAMPLE");
 console.log(transformString("HeLlO wOrLd") === "WoRlD hElLo");
-console.log(transformString(" HeLlO wOrLd ") === "WoRlD hElLo");
-console.log(transformString(" HeLlO  wOrLd ") === "WoRlD hElLo");
+console.log(transformString(" HeLlO wOrLd ") === " WoRlD hElLo ");
+console.log(transformString(" HeLlO  wOrLd ") === " WoRlD  hElLo ");
+console.log(transformString('  Maqm   Zn Mlr     Sdbe     Gwgh   Sh Irj'), 'iRJ sH   gWGH     sDBE     mLR zN   mAQM  ');
