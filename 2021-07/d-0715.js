fcbef4c29fcae92116da7456a395adff01ffb091
Toni Tesori <tt@tesoridesign.com>
2021-07-15 17:54:19 -0400

    Wayyy better solution

--- 2021-07/d-0715.js
+++ 2021-07/d-0715.js
@@ -3,29 +3,10 @@
 //Reverse the order of words from the input.
 //Note: You will have to handle multiple spaces, and leading/trailing spaces. You may assume the input only contain English alphabet and spaces.
 
-const transformString = (str) => {
-  let newStr = '';
-  let currWord = [];
-  for (let i = str.length - 1; i >= 0; i--) {
-    let char = str[i];
-    if (char === ' ' && currWord.length > 0) {
-      newStr += currWord.reverse().join('') + char;
-      currWord = [];
-    } else if (char === ' ') {
-      newStr += char;
-    } else {
-      char = char.charCodeAt(0) <= 90 ? char.toLowerCase() : char.toUpperCase();
-      currWord.push(char)
-    }
-  }
-  if (currWord.length > 0) {
-    newStr += currWord.reverse().join('');
-  }
-  return newStr;
-}
+const transformString = (str) => str.split(' ').reverse().join(' ').split('').map(letter => letter === letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase()).join('');
 
 console.log(transformString("Example Input") === "iNPUT eXAMPLE");
 console.log(transformString("HeLlO wOrLd") === "WoRlD hElLo");
 console.log(transformString(" HeLlO wOrLd ") === " WoRlD hElLo ");
 console.log(transformString(" HeLlO  wOrLd ") === " WoRlD  hElLo ");
-console.log(transformString('  Maqm   Zn Mlr     Sdbe     Gwgh   Sh Irj'), 'iRJ sH   gWGH     sDBE     mLR zN   mAQM  ');
+console.log(transformString('  Maqm   Zn Mlr     Sdbe     Gwgh   Sh Irj') === 'iRJ sH   gWGH     sDBE     mLR zN   mAQM  ');
