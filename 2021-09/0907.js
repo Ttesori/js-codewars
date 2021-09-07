21ba5b01b550b19e70c0e5bb6b62475c096e2f7d
Toni Tesori <tt@tesoridesign.com>
2021-09-07 14:08:15 -0400

    Today

---
+++ 2021-09/0907.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038
+function setAlarm(employed, vacation) {
+  return employed && !vacation;
+}
+
+// https://www.codewars.com/kata/59342039eb450e39970000a6
+function oddCount(n) {
+  return Math.floor(n / 2);
+}
+
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6
+function cockroachSpeed(s) {
+  return Math.floor(s * 27.7777778)
+}
