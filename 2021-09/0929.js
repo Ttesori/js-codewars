51946b6120a497d869abaeab8924eaf8158df7a8
Toni Tesori <tt@tesoridesign.com>
2021-09-29 13:59:01 -0400

    Today

---
+++ 2021-09/0929.js
@@ -0,0 +1,31 @@
+// https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+function helloWorld() {
+  let str = 'Hello World!';
+  console.log(str);
+}
+
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a/train/javascript
+class Kata {
+  static getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+  }
+}
+
+// https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/javascript
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+// https://www.codewars.com/kata/571d42206414b103dc0006a1/train/javascript
+const arr = N => {
+  let arr = [];
+  for (let i = 0; i < N; i++) {
+    arr.push(i);
+  }
+  return arr;
+}
+
+// https://www.codewars.com/kata/50654ddff44f800200000007/train/javascript
+function solution(a, b) {
+  return a.length > b.length ? b + a + b : a + b + a;
+}
