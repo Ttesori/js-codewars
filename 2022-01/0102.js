a4ac324d8cdfb946761ac5515e266daed5026b38
Toni Tesori <tt@tesoridesign.com>
2022-01-02 11:44:01 -0500

    2nd problem

--- 2022-01/0102.js
+++ 2022-01/0102.js
@@ -2,3 +2,9 @@
 function remove(s) {
   return s.replace(/!/g, '') + '!';
 }
+
+// https://www.codewars.com/kata/557af9418895e44de7000053
+var repeatIt = function (str, n) {
+  return typeof str === "string" ? str.repeat(n) : 'Not a string';
+}
+
