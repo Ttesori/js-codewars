63b2eaeec0a1db18f0b3e20a4336a144b27d103a
Toni Tesori <tt@tesoridesign.com>
2022-01-03 09:26:14 -0500

    Problem 2

--- 2022-01/0103.js
+++ 2022-01/0103.js
@@ -3,3 +3,28 @@ function validateCode(code) {
   let reg = new RegExp('^[1,2,3]')
   return reg.test(code);
 }
+
+// https://www.codewars.com/kata/572059afc2f4612825000d8a
+function howManydays(month) {
+  var days;
+  switch (month) {
+    case 2:
+      days = 28;
+      break;
+    case 4:
+      days = 30;
+      break;
+    case 6:
+      days = 30;
+      break;
+    case 9:
+      days = 30;
+      break;
+    case 11:
+      days = 30;
+      break;
+    default:
+      days = 31;
+  }
+  return days;
+}
