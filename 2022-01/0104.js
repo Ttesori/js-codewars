b354b263148a68c0a6b4687c53fe5c57acc81093
Toni Tesori <tt@tesoridesign.com>
2022-01-04 09:18:05 -0500

    Problem 3

--- 2022-01/0104.js
+++ 2022-01/0104.js
@@ -9,3 +9,14 @@ function whoIsPaying(name) {
 function sayHello(name, city, state) {
   return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
 }
+
+
+//https://www.codewars.com/kata/57158fb92ad763bb180004e7/train/javascript
+function rainAmount(mm) {
+  if (mm < 40) {
+    return "You need to give your plant " + (40 - mm) + "mm of water"
+  }
+  else {
+    return "Your plant has had more than enough water for today!"
+  };
+}
