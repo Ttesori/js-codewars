bb2a3820aa17105269a6c35a46ba3434b5ce5659
Toni Tesori <tt@tesoridesign.com>
2022-01-04 09:16:15 -0500

    Problem 2

--- 2022-01/0104.js
+++ 2022-01/0104.js
@@ -3,3 +3,9 @@
 function whoIsPaying(name) {
   return name.length < 3 ? [name] : [name, name.slice(0, 2)];
 }
+
+
+// https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
+function sayHello(name, city, state) {
+  return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+}
