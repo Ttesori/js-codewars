7156e3e9672c7e9c2da28f806226d012c51329c3
Toni Tesori <tt@tesoridesign.com>
2022-01-06 11:37:28 -0500

    Problem 1 fix

--- 2022-01/0106.js
+++ 2022-01/0106.js
@@ -1,4 +1,6 @@
 // https://www.codewars.com/kata/570597e258b58f6edc00230d/train/javascript
 function array(arr) {
-  return arr.length > 3 ? arr.split(',').slice(1, -1).join(' ') : null;
+  arr = arr.split(',');
+  return arr.length > 2 ? arr.slice(1, -1).join(' ') : null;
 }
+
