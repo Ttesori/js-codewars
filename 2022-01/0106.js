9402b36c317551dcb273c5d1e0329459586edbfc
Toni Tesori <tt@tesoridesign.com>
2022-01-06 11:38:51 -0500

    Problem 2

--- 2022-01/0106.js
+++ 2022-01/0106.js
@@ -4,3 +4,8 @@ function array(arr) {
   return arr.length > 2 ? arr.slice(1, -1).join(' ') : null;
 }
 
+// https://www.codewars.com/kata/59bd5dc270a3b7350c00008b/train/javascript
+function checkTheBucket(bucket) {
+  return bucket.includes('gold');
+}
+
