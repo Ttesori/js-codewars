2a010ecb2b37bfebbf58ecce9f0952c0c648b894
Toni Tesori <tt@tesoridesign.com>
2022-01-07 16:22:49 -0500

    Problem 3

--- 2022-01/0107.js
+++ 2022-01/0107.js
@@ -13,3 +13,14 @@ function calculateYears(principal, interest, tax, desired) {
   }
   return years;
 }
+
+// https://www.codewars.com/kata/5813d19765d81c592200001a/train/javascript
+function dontGiveMeFive(start, end) {
+  let count = 0;
+  for (let i = start; i <= end; i++) {
+    if (!String(i).includes('5')) {
+      count++;
+    }
+  }
+  return count;
+}
