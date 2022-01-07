ba06d6da2c426d38406d2d6448ebddfd7458c2be
Toni Tesori <tt@tesoridesign.com>
2022-01-07 16:17:18 -0500

    Problem 2

--- 2022-01/0107.js
+++ 2022-01/0107.js
@@ -2,3 +2,14 @@
 function minMax(arr) {
   return [Math.min(...arr), Math.max(...arr)];
 }
+
+// https://www.codewars.com/kata/563f037412e5ada593000114/train/javascript
+function calculateYears(principal, interest, tax, desired) {
+  let years = 0;
+  while (principal < desired) {
+    years++;
+    const add = principal * interest;
+    principal += add - (add * tax)
+  }
+  return years;
+}
