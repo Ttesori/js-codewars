52c06585a015dbdc35bb261244bc07b654853ff3
Toni Tesori <tt@tesoridesign.com>
2022-01-11 18:16:05 -0500

    Problem 2

--- 2022-01/0111.js
+++ 2022-01/0111.js
@@ -9,3 +9,9 @@ function cookie(x) {
   }
   return `Who ate the last cookie? It was ${who}!`;
 }
+
+// https://www.codewars.com/kata/58d248c7012397a81800005c/train/javascript
+var cubeChecker = function (volume, side) {
+  if (volume < side || volume <= 0 || side <= 0) return false;
+  return volume === (Math.pow(side, 3));
+};
