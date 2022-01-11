8c6866868c5491115fc88a0b24869f84d67b0f22
Toni Tesori <tt@tesoridesign.com>
2022-01-11 18:12:03 -0500

    Problem 1

---
+++ 2022-01/0111.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/55a996e0e8520afab9000055/train/javascript
+function cookie(x) {
+  let who = 'the dog';
+  if (typeof x === 'string') {
+    who = 'Zach'
+  }
+  if (typeof x === 'number') {
+    who = 'Monica'
+  }
+  return `Who ate the last cookie? It was ${who}!`;
+}
