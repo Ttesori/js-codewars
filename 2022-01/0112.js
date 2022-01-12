f147523babfe644e1f53b952d94c486c591ea04f
Toni Tesori <tt@tesoridesign.com>
2022-01-12 11:08:44 -0500

    Problem 4

--- 2022-01/0112.js
+++ 2022-01/0112.js
@@ -14,3 +14,12 @@ function trueOrFalse(val) {
   if (!val) return "false";
   else return "true";
 }
+
+//https://www.codewars.com/kata/544aed4c4a30184e960010f4/train/javascript
+function divisors(integer) {
+  const divisors = [];
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) divisors.push(i);
+  }
+  return divisors.length > 0 ? divisors : `${integer} is prime`
+};
