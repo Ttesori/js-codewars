5a04e92749b48d3178d8e796eb5099b810550d3d
Toni Tesori <tt@tesoridesign.com>
2022-01-12 11:05:37 -0500

    Problem 2

--- 2022-01/0112.js
+++ 2022-01/0112.js
@@ -2,3 +2,9 @@
 function printArray(array) {
   return array.join()
 }
+
+// https://www.codewars.com/kata/5612e743cab69fec6d000077/train/javascript
+var a = "dev"
+var b = "Lab"
+
+var name = a + b
