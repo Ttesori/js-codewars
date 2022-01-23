773b9706145eb4381248199e23ed70545336e3a2
Toni Tesori <tt@tesoridesign.com>
2022-01-22 19:40:03 -0500

    Problem 2

--- 2022-01/0122.js
+++ 2022-01/0122.js
@@ -2,3 +2,8 @@
 function usdcny(usd) {
   return (usd * 6.75).toFixed(2) + ' Chinese Yuan';
 }
+
+// https://www.codewars.com/kata/57a37f3cbb99449513000cd8/train/javascript
+function getNumberFromString(s) {
+  return Number(s.match(/(\d+)/g).join(''));
+}
