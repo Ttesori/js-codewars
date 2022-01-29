46cfdf87c8251002cb6c7987556cf96a33084a86
Toni Tesori <tt@tesoridesign.com>
2022-01-29 17:02:40 -0500

    Problem 2

--- 2022-01/0129.js
+++ 2022-01/0129.js
@@ -1,2 +1,7 @@
 // https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
 const number = array => array.map((str, i) => `${i + 1}: ${str}`)
+
+//https://www.codewars.com/kata/534ea96ebb17181947000ada/train/javascript
+function breakChocolate(n, m) {
+  return n > 0 && m > 0 ? (n * m) - 1 : 0;
+}
