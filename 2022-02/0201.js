2b6557c01cf67f7c0d70ca72495f8726bfbf1c35
Toni Tesori <tt@tesoridesign.com>
2022-02-01 08:31:35 -0500

    Problem 3

--- 2022-02/0201.js
+++ 2022-02/0201.js
@@ -15,3 +15,8 @@ function getDivisorsCnt(n) {
   }
   return divisors;
 }
+
+// https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/javascript
+function sortByLength(array) {
+  return array.sort((a, b) => a.length - b.length);
+};
