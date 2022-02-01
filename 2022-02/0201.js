e2e2b47e458a890e4ca3de2232fe4f13f189a017
Toni Tesori <tt@tesoridesign.com>
2022-02-01 08:27:20 -0500

    Refactor 1

--- 2022-02/0201.js
+++ 2022-02/0201.js
@@ -1,12 +1,8 @@
 // https://www.codewars.com/kata/566fc12495810954b1000030/train/javascript
 function nbDig(n, d) {
-  let squares = [];
-  for (let i = 0; i <= n; i++) {
-    squares.push(i * i)
-  }
   let digits = 0;
-  squares.forEach(num => {
-    String(num).split('').forEach(digit => digit == d ? digits++ : false)
-  })
+  for (let i = 0; i <= n; i++) {
+    String(i * i).split('').forEach(digit => digit == d ? digits++ : false);
+  }
   return digits;
 }
