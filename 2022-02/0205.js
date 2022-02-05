afe8d74f75a59c259ce714370b84fb8b8975f140
Toni Tesori <tt@tesoridesign.com>
2022-02-05 13:49:00 -0500

    Today

---
+++ 2022-02/0205.js
@@ -0,0 +1,2 @@
+//https://www.codewars.com/kata/582dafb611d576b745000b74/train/javascript
+const quote = fighter => fighter.toLowerCase() === 'george saint pierre' ? 'I am not impressed by your performance.' : "I'd like to take this chance to apologize.. To absolutely NOBODY!"; 
