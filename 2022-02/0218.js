7ee77197ceed12af7302c5675853c203e66aaed0
Toni Tesori <tt@tesoridesign.com>
2022-02-18 13:52:21 -0500

    Problem 6

--- 2022-02/0218.js
+++ 2022-02/0218.js
@@ -46,5 +46,10 @@ function uniTotal(string) {
   return string.split('').reduce((count, char) => count += char.charCodeAt(0), 0);
 }
 
+// https://www.codewars.com/kata/570669d8cb7293a2d1001473/train/javascript
+function ifChuckSaysSo() {
+  return 1 === 0;
+}
+
 
 
