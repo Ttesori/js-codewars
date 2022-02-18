03f157eee80c3d113ecc745fee4f96c0142b7558
Toni Tesori <tt@tesoridesign.com>
2022-02-18 11:08:38 -0500

    Today

---
+++ 2022-02/0218.js
@@ -0,0 +1,32 @@
+function toCsvText(array) {
+  let output = '';
+  array.forEach((el, i) => {
+    output += el.join(',')
+    if (array.length - 1 !== i) output += '\n';
+  })
+  return output;
+}
+
+// https://www.codewars.com/kata/574b1916a3ebd6e4fa0012e7/train/javascript
+function isOpposite(s1, s2) {
+  if (s1.length === 0 || s2.length === 0 || s1.toLowerCase() !== s2.toLowerCase()) return false;
+  return s1.split('').every((el, i) => el.toUpperCase() === el ? el.toLowerCase() === s2[i] : el.toUpperCase() === s2[i]);
+}
+
+// https://www.codewars.com/kata/57613fb1033d766171000d60/train/javascript
+function uefaEuro2016(teams, scores) {
+  let output = `At match ${teams[0]} - ${teams[1]}, `;
+  if (scores[0] > scores[1]) {
+    return output + `${teams[0]} won!`;
+  } else if (scores[1] > scores[0]) {
+    return output + `${teams[1]} won!`;
+  } else {
+    return output + `teams played draw.`;
+  }
+}
+
+//
+function integrate(coefficient, exponent) {
+  return `${coefficient / (exponent + 1)}x^${exponent + 1}`;
+}
+
