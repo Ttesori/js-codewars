00ac99698afc2f06f185f5efdd6abb70a321b903
Toni Tesori <tt@tesoridesign.com>
2022-02-24 12:38:38 -0500

    Problem 3

--- 2022-02/0224.js
+++ 2022-02/0224.js
@@ -18,3 +18,9 @@ function correctPolishLetters(string) {
 function multiply(number) {
   return number * Math.pow(5, Math.abs(number).toString().length);
 }
+
+//https://www.codewars.com/kata/54147087d5c2ebe4f1000805/train/javascript
+function _if(bool, func1, func2) {
+  return bool ? func1() : func2();
+}
+
