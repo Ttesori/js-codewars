4f9983848b0be5f06d1d6cd8a9a61168c7a1c980
Toni Tesori <tt@tesoridesign.com>
2022-02-25 14:28:00 -0500

    Problem 2

--- 2022-02/0225.js
+++ 2022-02/0225.js
@@ -8,3 +8,13 @@ class Ship {
     return this.draft - (this.crew * 1.5) > 20;
   }
 }
+
+//https://www.codewars.com/kata/5722b3f0bd5583cf44001000/train/javascript
+function giveMeFive(obj) {
+  const arr = [];
+  for (let key in obj) {
+    if (key.length === 5) arr.push(key);
+    if (obj[key].length === 5) arr.push(obj[key]);
+  }
+  return arr;
+}
