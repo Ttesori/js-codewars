c82810c9fb0b44a095166d7051d694e87535599c
Toni Tesori <tt@tesoridesign.com>
2022-02-25 14:43:43 -0500

    Problem 4

--- 2022-02/0225.js
+++ 2022-02/0225.js
@@ -24,4 +24,6 @@ function splitAndMerge(str, separator) {
   return str.split(' ').map(word => word.split('').join(separator)).join(' ');
 }
 
+//https://www.codewars.com/kata/56f3f6a82010832b02000f38/train/javascript
+const describeAge = (age) => `You're a(n) ${age <= 12 ? 'kid' : age >= 13 && age <= 17 ? 'teenager' : age <= 64 ? 'adult' : 'elderly'}`;
 
