7c44e4ec2ca82638ea11a849b33af7ea42dd2082
Toni Tesori <tt@tesoridesign.com>
2022-02-26 15:44:45 -0500

    Moar problems

--- 2022-02/0226.js
+++ 2022-02/0226.js
@@ -41,3 +41,10 @@ class Cat extends Animal {
   }
 }
 
+//https://www.codewars.com/kata/5713bc89c82eff33c60009f7/train/javascript
+function toFreud(string) {
+  return string.length > 0 ? string.split(' ').map(word => 'sex').join(' ') : '';
+}
+
+//https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
+websites.push('codewars');
