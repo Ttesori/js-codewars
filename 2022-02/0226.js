cd41469e7c8acc7e769f67468834e40072d52fb9
Toni Tesori <tt@tesoridesign.com>
2022-02-26 15:39:41 -0500

    Problem 3

--- 2022-02/0226.js
+++ 2022-02/0226.js
@@ -14,3 +14,22 @@ function evalObject(value) {
 function datingRange(age) {
   return age >= 14 ? `${Math.floor((age / 2) + 7)}-${Math.floor((age - 7) * 2)}` : `${Math.floor(age - .10 * age)}-${Math.floor(age + .10 * age)}`;
 }
+
+//https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/javascript
+var rooms = {
+  room1: {
+    name: 'room 1',
+    desc: 'desc 1',
+    completed: false
+  },
+  room2: {
+    name: 'room 2',
+    desc: 'desc 2',
+    completed: false
+  },
+  room3: {
+    name: 'room 3',
+    desc: 'desc 3',
+    completed: false
+  }
+}
