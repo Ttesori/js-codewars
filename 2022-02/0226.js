9e1e17be78cb9f7255945fcfa6b4585c823653ef
Toni Tesori <tt@tesoridesign.com>
2022-02-26 15:42:14 -0500

    Problem 4

--- 2022-02/0226.js
+++ 2022-02/0226.js
@@ -33,3 +33,11 @@ var rooms = {
     completed: false
   }
 }
+
+//https://www.codewars.com/kata/55a14aa4817efe41c20000bc/train/javascript
+class Cat extends Animal {
+  speak() {
+    return `${this.name} meows.`;
+  }
+}
+
