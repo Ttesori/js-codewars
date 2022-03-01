97edf97aa06ecdf9d1171319f8f51653086c8d59
Toni Tesori <tt@tesoridesign.com>
2022-03-01 12:51:32 -0500

    Problem 3

--- 2022-03/0301.js
+++ 2022-03/0301.js
@@ -7,3 +7,9 @@ const isReallyNaN = (val) => {
 function firstToLast(str, c) {
   return str.indexOf(c) !== -1 ? str.lastIndexOf(c) - str.indexOf(c) : -1;
 }
+
+//https://www.codewars.com/kata/56d6c333c9ae3fc32800070f/train/javascript
+function yearDays(year) {
+  const days = year % 100 === 0 && year % 400 === 0 ? 366 : year % 4 === 0 && year % 100 !== 0 ? 366 : 365;
+  return `${year} has ${days} days`;
+}
