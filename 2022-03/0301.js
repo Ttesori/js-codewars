a2a20b65c84e414cdd856b5bfa06ce0f3e044457
Toni Tesori <tt@tesoridesign.com>
2022-03-01 12:45:19 -0500

    Problem 2

--- 2022-03/0301.js
+++ 2022-03/0301.js
@@ -2,3 +2,8 @@
 const isReallyNaN = (val) => {
   return Number.isNaN(val);
 };
+
+//https://www.codewars.com/kata/57277a31e5e51450a4000010/train/javascript
+function firstToLast(str, c) {
+  return str.indexOf(c) !== -1 ? str.lastIndexOf(c) - str.indexOf(c) : -1;
+}
