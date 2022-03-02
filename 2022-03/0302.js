8c2e8fbb56504cefe4ef6c7796d015ccfb7bd2a3
Toni Tesori <tt@tesoridesign.com>
2022-03-02 10:54:55 -0500

    Problem 2

--- 2022-03/0302.js
+++ 2022-03/0302.js
@@ -4,3 +4,8 @@ function bigToSmall(arr) {
   flat.sort((a, b) => b - a);
   return flat.join('>');
 }
+
+//https://www.codewars.com/kata/570bcd9715944a2c8e000009/train/javascript
+function sc(floors) {
+  return floors <= 1 ? "" : 'Aa~ '.repeat(floors - 1) + (floors <= 6 ? 'Pa! Aa!' : 'Pa!');
+}
