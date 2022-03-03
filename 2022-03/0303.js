62f161a83ab89d0ddb55955ecc1147dfed4c1ab2
Toni Tesori <tt@tesoridesign.com>
2022-03-03 13:09:00 -0500

    Problem 2

--- 2022-03/0303.js
+++ 2022-03/0303.js
@@ -2,3 +2,14 @@
 function logs(x, a, b) {
   return Math.log(a * b) / Math.log(x);
 }
+
+//https://www.codewars.com/kata/55e8aba23d399a59500000ce/train/javascript
+class Hero {
+  constructor(name = 'Hero') {
+    this.name = name;
+    this.position = '00';
+    this.health = 100;
+    this.damage = 5;
+    this.experience = 0;
+  }
+}
