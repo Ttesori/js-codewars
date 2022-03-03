aa4c807b8b2896529c1f3b6c8d21d515f26c6b6b
Toni Tesori <tt@tesoridesign.com>
2022-03-03 13:29:54 -0500

    Problem 5

--- 2022-03/0303.js
+++ 2022-03/0303.js
@@ -37,3 +37,9 @@ function HQ9(code) {
     return lyrics;
   }
 }
+
+//https://www.codewars.com/kata/56a4addbfd4a55694100001f/train/javascript
+function validateHello(greetings) {
+  res = /hello|ciao|salut|hallo|hola|ahoj|czesc/.test(greetings.toLowerCase())
+  return res
+}
