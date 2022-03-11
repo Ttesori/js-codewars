aedf198ea32e693e78003027feadc28457a11e63
Toni Tesori <tt@tesoridesign.com>
2022-03-11 14:21:39 -0500

    Prob 5

--- 2022-03/03.js
+++ 2022-03/03.js
@@ -59,3 +59,6 @@ function main() {
   buyHealth();
   printStatus();
 }
+
+//https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript
+const ArrowFunc = (arr) => arr.map(num => String.fromCharCode(num)).join(''); 
