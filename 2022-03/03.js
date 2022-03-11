af49871d8c33f304969ba9751d5974ebdc8dc6f8
Toni Tesori <tt@tesoridesign.com>
2022-03-11 14:11:01 -0500

    Problem 2

--- 2022-03/03.js
+++ 2022-03/03.js
@@ -21,3 +21,16 @@ const find = (arr, el) => arr.indexOf(el) === -1 ? "Not found" : arr.indexOf(el)
 
 //https://www.codewars.com/kata/559f860f8c0d6c7784000119/train/javascript
 const anyArrows = (arrows) => arrows.some(arrow => !arrow.hasOwnProperty('damaged') || !arrow.damaged);
+
+//https://www.codewars.com/kata/562e98755e9214cd2500003d/train/javascript
+function yourFutureCareer() {
+  var career = Math.random();
+  if (career <= 0.32) {
+    return 'FrontEnd Developer';
+  } else if (career <= 0.65) {
+    return 'BackEnd Developer';
+  } else {
+    return 'Full-Stack Developer';
+  }
+}
+yourFutureCareer();
