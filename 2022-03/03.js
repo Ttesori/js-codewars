0c28ccfcf708f2d8abd6c420f02f42ec3f491ef1
Toni Tesori <tt@tesoridesign.com>
2022-03-11 14:17:28 -0500

    Prob 3

--- 2022-03/03.js
+++ 2022-03/03.js
@@ -34,3 +34,14 @@ function yourFutureCareer() {
   }
 }
 yourFutureCareer();
+
+//https://www.codewars.com/kata/5729b103dd8bac11a900119e/train/javascript
+function fiveLine(s) {
+  s = s.trim();
+  let str = '';
+  for (let i = 1; i <= 5; i++) {
+    str += s.repeat(i);
+    if (i !== 5) str += '\n';
+  }
+  return str;
+}
