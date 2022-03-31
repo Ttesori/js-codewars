e95dee5becab7390eea0812d193f6b9917afcc8c
Toni Tesori <tt@tesoridesign.com>
2022-03-31 09:27:05 -0400

    Prob 2

--- 2022-03/03.js
+++ 2022-03/03.js
@@ -164,3 +164,21 @@ function whatNumberIsIt(n) {
   const str = n === nums[0] ? 'Number.MAX_VALUE' : n === nums[1] ? 'Number.MIN_VALUE' : n === nums[2] ? 'Number.NEGATIVE_INFINITY' : n === nums[3] ? 'Number.POSITIVE_INFINITY' : isNaN(n) ? 'Number.NaN' : n;
   return `Input number is ${str}`;
 }
+
+//https://www.codewars.com/kata/56f7f8215d7c12c0e7000b19/train/javascript
+class Person {
+  constructor(firstName = 'John', lastName = 'Doe', age = 0, gender = 'Male') {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`;
+  }
+}
