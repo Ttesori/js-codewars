cd490ac63820e89f7932a124c3fc6b538a058b3d
Toni Tesori <tt@tesoridesign.com>
2022-04-18 10:40:09 -0400

    Prob 1

--- 2022-04/04.js
+++ 2022-04/04.js
@@ -76,3 +76,13 @@ Object.freeze(MrFreeze);
 function generateLink(user) {
   return `http://www.codewars.com/users/${encodeURIComponent(user)}`;
 }
+
+//https://www.codewars.com/kata/56019d3b2c39ccde76000086/train/javascript
+function doTurn() {
+  rollDice();
+  move();
+  combat();
+  getCoins();
+  buyHealth();
+  printStatus();
+}
