652cde149cfa4b32a48fc1d3d973d6ceced0b770
Toni Tesori <tt@tesoridesign.com>
2022-04-19 08:08:32 -0400

    Prob 2

--- 2022-04/04.js
+++ 2022-04/04.js
@@ -113,3 +113,8 @@ function logicalCalc(arr, op) {
   }
   return Boolean(curr);
 }
+
+//https://www.codewars.com/kata/55a75e2d0803fea18f00009d/train/javascript
+function slope(points) {
+  return points[2] - points[0] !== 0 ? `${(points[3] - points[1]) / (points[2] - points[0])}` : "undefined";
+}
