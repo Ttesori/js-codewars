f61033858e004df73be05b52a6b380613b3ba972
Toni Tesori <tt@tesoridesign.com>
2022-04-27 13:33:49 -0400

    Prob 1

--- 2022-04/04.js
+++ 2022-04/04.js
@@ -174,3 +174,8 @@ const a = '0';
 function isTriangle(a, b, c) {
   return a + b > c && a + c > b && c + b > a;
 }
+
+//https://www.codewars.com/kata/55d1d6d5955ec6365400006d/train/javascript
+function roundToNext5(n) {
+  return Math.ceil(n / 5) * 5;
+}
