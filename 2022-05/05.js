1dc42c10a23b5a6b5bc12ffdcc5d7e2b302c918e
Toni Tesori <tt@tonitesori.dev>
2022-05-05 14:50:22 -0400

    Prob 1

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -3,3 +3,8 @@ function factorial(n) {
   if (n > 12 || n < 0) throw new RangeError('');
   return n === 0 ? 1 : n * factorial(n - 1);
 }
+
+//https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+function solution(nums) {
+  return nums ? nums.sort((a, b) => a - b) : [];
+}
