f3825fe1b365afe299e0a8e0ecc25fc78d492695
Toni Tesori <tt@tonitesori.dev>
2022-05-06 12:11:21 -0400

    Prob 2

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -16,3 +16,8 @@ const sumDigits = (number) => Math.abs(number).toString().split('').reduce((sum,
 function reverseLetter(str) {
   return str.match(/[a-z]/g).reverse().join('');
 }
+
+//https://www.codewars.com/kata/57cc981a58da9e302a000214/train/javascript
+function smallEnough(a, limit) {
+  return a.every(num => num <= limit);
+}
