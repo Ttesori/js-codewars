f2389079334b3869ec861ab58782549bee1c42c4
Toni Tesori <tt@tonitesori.dev>
2022-05-11 09:17:29 -0400

    Prob 1

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -90,3 +90,13 @@ function SubtractSum(n) {
   }
   return list[n];
 }
+
+//https://www.codewars.com/kata/59c8b38423dacc7d95000008/train/javascript
+function isValid(formula) {
+  if (formula.includes(1) && formula.includes(2)) return false;
+  if (formula.includes(3) && formula.includes(4)) return false;
+  if (formula.includes(5) && !formula.includes(6)) return false;
+  if (formula.includes(6) && !formula.includes(5)) return false;
+  if (!formula.includes(7) && !formula.includes(8)) return false;
+  return true;
+}
