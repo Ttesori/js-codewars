9c2da34269ca0fd2db3e4d7a518cde3e0016b587
Toni Tesori <tt@tonitesori.dev>
2022-05-12 11:17:54 -0400

    Another

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -241,3 +241,10 @@ function changeMe(moneyIn) {
     default: return moneyIn;
   }
 }
+
+//https://www.codewars.com/kata/56e6a330715e7221d9000a3b/train/javascript
+Number.prototype.times = function (f) {
+  for (let i = 0; i < this; i++) {
+    f(i);
+  }
+};
