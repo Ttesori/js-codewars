123037de7dfe1fe8ccfe0fc6622fac1996f91775
Toni Tesori <tt@tonitesori.dev>
2022-05-12 20:14:55 -0400

    I DID ITTTTTTTTTTTTT

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -283,3 +283,6 @@ const configuration = {
   "name": "your-pack-name",
   "version": "1.0.0"
 };
+
+//https://www.codewars.com/kata/572269697297444c570008b9/train/javascript
+const greaterThanLessThan = (a, b, c) => a < b < c;
