4c4674fde63058bb47176f56267a52d802f519a7
Toni Tesori <tt@tonitesori.dev>
2022-05-13 13:24:04 -0400

    Three

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -296,3 +296,13 @@ function capitalize(s) {
 function removeDuplicateWords(s) {
   return Array.from(new Set(s.split(' '))).join(' ');
 }
+
+//https://www.codewars.com/kata/5663f5305102699bad000056/train/javascript
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) return -1;
+  a1.sort((a, b) => a.length - b.length);
+  a2.sort((a, b) => b.length - a.length);
+  const one = Math.abs(a1[0].length - a2[0].length);
+  const two = Math.abs(a1[a1.length - 1].length - a2[a2.length - 1].length);
+  return one > two ? one : two;
+}
