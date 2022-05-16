a89800f3f77cda68b2b93d3971a19c755ade426e
Toni Tesori <tt@tonitesori.dev>
2022-05-16 12:23:24 -0400

    Prob 2

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -321,3 +321,8 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
     defender = temp;
   }
 }
+
+// https://www.codewars.com/kata/5aba780a6a176b029800041c/train/javascript
+function maxMultiple(divisor, bound) {
+  return (Math.floor(bound / divisor) * divisor);
+}
