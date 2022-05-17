ae04d7818373e5f404f421c22f06c260b1c6578e
Toni Tesori <tt@tonitesori.dev>
2022-05-17 15:23:23 -0400

    Prob 1

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -337,3 +337,8 @@ function solution(digits) {
   }
   return max;
 }
+
+//https://www.codewars.com/kata/539de388a540db7fec000642/train/javascript
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  return enteredCode === correctCode && new Date(currentDate).getTime() <= new Date(expirationDate).getTime();
+}
