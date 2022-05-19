0261b47c3acf8486e689ca365549e22f1bba44a9
Toni Tesori <tt@tonitesori.dev>
2022-05-19 13:38:51 -0400

    PRob 2

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -378,3 +378,8 @@ function evaporator(content, evap_per_day, threshold) {
 function angle(n) {
   return (n - 2) * 180;
 }
+
+//https://www.codewars.com/kata/585a1a227cb58d8d740001c3/train/javascript
+function repeater(string, n) {
+  return string.repeat(n);
+}
