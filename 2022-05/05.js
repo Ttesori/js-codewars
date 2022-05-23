7fa5989d295821fbc13211af01e4e5a974a6616b
Toni Tesori <tt@tonitesori.dev>
2022-05-23 10:41:03 -0400

    Prob 2

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -417,3 +417,8 @@ function wordsToMarks(string) {
   const chars = '/abcdefghijklmnopqrstuvwxyz';
   return string.split('').reduce((sum, char) => sum + chars.indexOf(char), 0);
 }
+
+//https://www.codewars.com/kata/525e5a1cb735154b320002c8/train/javascript
+function triangular(n) {
+  return n > 0 ? (n * (n + 1)) / 2 : 0;
+}
