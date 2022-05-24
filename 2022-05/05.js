8a19523625e7b8c7007ebf119f311a6d614e027f
Toni Tesori <tt@tonitesori.dev>
2022-05-24 12:12:24 -0400

    Prob 3

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -437,3 +437,8 @@ function reverseNumber(n) {
 function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
   return Math.floor(Math.sqrt((age1 ** 2 + age2 ** 2 + age3 ** 2 + age4 ** 2 + age5 ** 2 + age6 ** 2 + age7 ** 2 + age8 ** 2)) / 2);
 }
+
+//https://www.codewars.com/kata/58daa7617332e59593000006/train/javascript
+function findLongest(array) {
+  return array.sort((a, b) => b.toString().length - a.toString().length)[0];
+}
