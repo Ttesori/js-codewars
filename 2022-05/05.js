0a4cbc3f532c6e54709fbd64d744f1079bccd7df
Toni Tesori <tt@tonitesori.dev>
2022-05-24 12:07:10 -0400

    Prob 1

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -427,3 +427,8 @@ function triangular(n) {
 function getEvenNumbers(numbersArray) {
   return numbersArray.filter(n => n % 2 === 0);
 }
+
+//https://www.codewars.com/kata/555bfd6f9f9f52680f0000c5/train/javascript
+function reverseNumber(n) {
+  return n > 0 ? Number(n.toString().split('').reverse().join('')) : -Number(Math.abs(n).toString().split('').reverse().join(''));
+}
