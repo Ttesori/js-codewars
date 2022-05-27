663aace3b842d35e8259ddc030c67cb33e388909
Toni Tesori <tt@tonitesori.dev>
2022-05-27 13:42:36 -0400

    Prob 3

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -519,3 +519,8 @@ function fizzbuzz(n) {
   }
   return output;
 }
+
+//https://www.codewars.com/kata/51fd6bc82bc150b28e0000ce/train/javascript
+function noOdds(values) {
+  return values.filter(num => num % 2 === 0);
+}
