793f0f40c159ea12c676b3e8d52262899c9de65e
Toni Tesori <tt@tonitesori.dev>
2022-05-31 19:28:37 -0400

    Prob 1

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -535,3 +535,8 @@ function halvingSum(n) {
   }
   return sum;
 }
+
+//https://www.codewars.com/kata/5a431c0de1ce0ec33a00000c/train/javascript
+function evenNumbers(array, number) {
+  return array.filter(n => n % 2 === 0).slice(-number);
+}
