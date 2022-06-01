b7adf35f65c839260d2968d89f2b91498136360a
Toni Tesori <tt@tonitesori.dev>
2022-06-01 10:29:28 -0400

    Move to june

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -540,21 +540,3 @@ function halvingSum(n) {
 function evenNumbers(array, number) {
   return array.filter(n => n % 2 === 0).slice(-number);
 }
-
-//https://www.codewars.com/kata/59d9ff9f7905dfeed50000b0/train/javascript
-function solve(arr) {
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-  return arr.map(word => word.split('').reduce((sum, char, i) => alphabet[i] === char.toLowerCase() ? sum + 1 : sum, 0));
-};
-
-//https://www.codewars.com/kata/534d0a229345375d520006a0/train/javascript
-function isPowerOfTwo(n) {
-  let current = 0;
-  let i = 0;
-  while (current <= n) {
-    current = 2 ** i;
-    i++;
-    if (current === n) return true;
-  }
-  return false;
-}
