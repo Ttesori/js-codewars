005b068bb5a8f581b0d1b9f264b08fb6331dd136
Toni Tesori <tt@tonitesori.dev>
2022-06-01 10:29:09 -0400

    Prob 2

--- 2022-05/05.js
+++ 2022-05/05.js
@@ -546,3 +546,15 @@ function solve(arr) {
   const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
   return arr.map(word => word.split('').reduce((sum, char, i) => alphabet[i] === char.toLowerCase() ? sum + 1 : sum, 0));
 };
+
+//https://www.codewars.com/kata/534d0a229345375d520006a0/train/javascript
+function isPowerOfTwo(n) {
+  let current = 0;
+  let i = 0;
+  while (current <= n) {
+    current = 2 ** i;
+    i++;
+    if (current === n) return true;
+  }
+  return false;
+}
