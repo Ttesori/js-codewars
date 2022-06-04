eb56775224981562bfdfd2bfb7477c6114f1953c
Toni Tesori <tt@tonitesori.dev>
2022-06-04 09:39:27 -0400

    Prob 1

--- 2022-06/06.js
+++ 2022-06/06.js
@@ -69,3 +69,8 @@ function partlist(arr) {
   }
   return output;
 }
+
+//https://www.codewars.com/kata/57d2807295497e652b000139/train/javascript
+function averages(numbers) {
+  return !numbers || numbers.length < 2 ? [] : numbers.map((num, i) => (num + numbers[i + 1]) / 2).slice(0, numbers.length - 1);
+}
