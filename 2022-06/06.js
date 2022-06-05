918a0f1ea50270f206f06a0a70740c18dd2556dd
Toni Tesori <tt@tonitesori.dev>
2022-06-05 09:43:07 -0400

    PRob 2

--- 2022-06/06.js
+++ 2022-06/06.js
@@ -79,3 +79,8 @@ function averages(numbers) {
 function hasUniqueChars(str) {
   return str === [...new Set(str.split(''))].join('').toString();
 }
+
+//https://www.codewars.com/kata/52aeb2f3ad0e952f560005d3/train/javascript
+function sortGiftCode(code) {
+  return code.split('').sort().join('');
+}
