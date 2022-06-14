6d824dc78303a6e48c78b52803d59c6fef75bca8
Toni Tesori <tt@tonitesori.dev>
2022-06-14 11:39:44 -0400

    Prob - made it to 1600!

--- 2022-06/06.js
+++ 2022-06/06.js
@@ -211,3 +211,8 @@ function triangle(row) {
   }
   return row;
 }
+
+//https://www.codewars.com/kata/52b5247074ea613a09000164/train/javascript
+function cookingTime(eggs) {
+  return Math.ceil(eggs / 8) * 5;
+}
