aa35120169eec1eac96078e597a4f4216d42b7bb
Toni Tesori <tt@tonitesori.dev>
2022-06-15 13:33:36 -0400

    Prob 2

--- 2022-06/06.js
+++ 2022-06/06.js
@@ -234,3 +234,8 @@ function balancedNum(number) {
 
   return left === right ? 'Balanced' : 'Not Balanced';
 }
+
+//https://www.codewars.com/kata/588a3c3ef0fbc9c8e1000095/train/javascript
+function maxDiff(list) {
+  return list.length > 1 ? Math.max(...list) - Math.min(...list) : 0;
+};
