88631a28ca977f2be3850ad0894d34fc184f7cc7
Toni Tesori <tt@tonitesori.dev>
2022-06-17 12:18:38 -0400

    Another

--- 2022-06/06.js
+++ 2022-06/06.js
@@ -363,3 +363,12 @@ function longestConsec(arr, k) {
   }
   return longestStr;
 }
+
+//https://www.codewars.com/kata/5262119038c0985a5b00029f/train/javascript
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
