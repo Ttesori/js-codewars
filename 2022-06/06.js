ffb9b3104fe4512e757c55ea23ba6fb96350f3d2
Toni Tesori <tt@tonitesori.dev>
2022-06-27 12:24:06 -0400

    Prob 2

--- 2022-06/06.js
+++ 2022-06/06.js
@@ -411,3 +411,11 @@ class Dinglemouse {
     return `${this.firstName}${this.firstName && this.lastName ? ' ' : ''}${this.lastName}`;
   }
 }
+//https://www.codewars.com/kata/5413759479ba273f8100003d/train/javascript
+reverse = function (array) {
+  let arr = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    arr.push(array[i]);
+  }
+  return arr;
+};
