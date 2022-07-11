3311029870746f889d79102f56af507e42eaf6c0
Toni Tesori <tt@tonitesori.dev>
2022-07-11 11:08:14 -0400

    Today

--- 2022-07/07.js
+++ 2022-07/07.js
@@ -32,3 +32,8 @@ function multiplyAll(arr) {
     return arr.map(el => el * num);
   };
 }
+
+//https://www.codewars.com/kata/57f8ee485cae443c4d000127/train/javascript
+function spacify(str) {
+  return str.split('').join(' ');
+}
