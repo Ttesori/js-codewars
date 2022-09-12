332f04a1d23e1bc2dd30a2aaa5162f8dd1713c08
Toni Tesori <tt@tonitesori.dev>
2022-09-12 11:25:23 -0400

    Today

--- 2022-09/09.js
+++ 2022-09/09.js
@@ -5,3 +5,19 @@ function maxTriSum(numbers) {
   nums2.sort((a, b) => b - a);
   return nums2.slice(0, 3).reduce((sum, num) => sum + num, 0);
 }
+
+//https://www.codewars.com/kata/57ecf6efc7fe13eb070000e1/train/javascript
+function outed(meet, boss) {
+  let total = 0;
+  let people = 0;
+  for (let person in meet) {
+    if (person === boss) {
+      total += meet[person] * 2;
+    } else {
+      total += meet[person];
+    }
+    people++;
+  }
+  let avg = total / people;
+  return avg <= 5 ? 'Get Out Now!' : 'Nice Work Champ!';
+}
