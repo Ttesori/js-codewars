fee0af6e564732b6ef64bd1c3056dd303420dea3
Toni Tesori <tt@tonitesori.dev>
2022-09-14 10:39:33 -0400

    Today

--- 2022-09/09.js
+++ 2022-09/09.js
@@ -26,3 +26,8 @@ function outed(meet, boss) {
 function tidyNumber(n) {
   return n === Number(n.toString().split('').sort((a, b) => Number(a) - Number(b)).join(''));
 }
+
+//https://www.codewars.com/kata/5a63948acadebff56f000018/train/javascript
+function maxProduct(numbers, size) {
+  return numbers.sort((a, b) => b - a).slice(0, size).reduce((prod, num) => prod * num, 1);
+}
