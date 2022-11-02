8b3d4f947c819e776923f781a99c286ad606dabd
Toni Tesori <tt@tesoridesign.com>
2022-11-02 19:19:29 -0400

    Problem

--- 2021-11/2021-11.js
+++ 2021-11/2021-11.js
@@ -3,3 +3,8 @@ function yearDays(year) {
   const days = year % 100 === 0 && year % 400 === 0 ? 366 : year % 4 === 0 && year % 100 !== 0 ? 366 : 365;
   return `${year} has ${days} days`;
 }
+
+//https://www.codewars.com/kata/57277a31e5e51450a4000010/train/javascript
+function firstToLast(str, c) {
+  return str.indexOf(c) !== -1 ? str.lastIndexOf(c) - str.indexOf(c) : -1;
+}
