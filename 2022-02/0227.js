808e8d0d010d040b9dea08c6e5d304a4330a391b
Toni Tesori <tt@tesoridesign.com>
2022-11-04 19:20:03 -0400

    Problem

--- 2022-02/0227.js
+++ 2022-02/0227.js
@@ -2,3 +2,9 @@
 function SafeInteger(n) {
   return Number.isSafeInteger(n);
 }
+
+//https://www.codewars.com/kata/578c1e2edaa01a9a02000b7f/train/javascript
+function aliasGen(fname, lname) {
+  const check = new RegExp(/^[A-Za-z]/)
+  return check.test(fname) && check.test(lname) ? `${firstName[fname[0].toUpperCase()]} ${surname[lname[0].toUpperCase()]}` : 'Your name must start with a letter from A - Z.';
+}
