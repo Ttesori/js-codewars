04162b57ad139ae7def74a70febd5077ab6166bd
Toni Tesori <tt@tesoridesign.com>
2022-11-09 19:21:13 -0500

    Problem

--- 2021-11/2021-11.js
+++ 2021-11/2021-11.js
@@ -21,3 +21,8 @@ function ensureQuestion(s) {
 
 //https://www.codewars.com/kata/511f0fe64ae8683297000001/train/javascript
 websites.push('codewars');
+
+//https://www.codewars.com/kata/5713bc89c82eff33c60009f7/train/javascript
+function toFreud(string) {
+  return string.length > 0 ? string.split(' ').map(word => 'sex').join(' ') : '';
+}
