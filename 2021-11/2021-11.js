f4db6648f97280ab3507cbe61661477dd70bb46c
Toni Tesori <tt@tesoridesign.com>
2022-11-11 19:21:54 -0500

    Problem

--- 2021-11/2021-11.js
+++ 2021-11/2021-11.js
@@ -52,3 +52,8 @@ var rooms = {
     completed: false
   }
 }
+
+//https://www.codewars.com/kata/5803956ddb07c5c74200144e/train/javascript
+function datingRange(age) {
+  return age >= 14 ? `${Math.floor((age / 2) + 7)}-${Math.floor((age - 7) * 2)}` : `${Math.floor(age - .10 * age)}-${Math.floor(age + .10 * age)}`;
+}
