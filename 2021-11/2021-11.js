e13d850b21acda275b429c1764b5abe7795bcbf6
Toni Tesori <tt@tesoridesign.com>
2022-11-14 19:22:09 -0500

    Problem

--- 2021-11/2021-11.js
+++ 2021-11/2021-11.js
@@ -57,3 +57,15 @@ var rooms = {
 function datingRange(age) {
   return age >= 14 ? `${Math.floor((age / 2) + 7)}-${Math.floor((age - 7) * 2)}` : `${Math.floor(age - .10 * age)}-${Math.floor(age + .10 * age)}`;
 }
+
+//https://www.codewars.com/kata/55c933c115a8c426ac000082/train/javascript
+function evalObject(value) {
+  switch (value.operation) {
+    case '+': return value.a + value.b;
+    case '-': return value.a - value.b;
+    case '/': return value.a / value.b;
+    case '*': return value.a * value.b;
+    case '%': return value.a % value.b;
+    case '^': return Math.pow(value.a, value.b);
+  }
+}
