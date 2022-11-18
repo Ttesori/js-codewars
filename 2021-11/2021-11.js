d0b54dec4f0a5c0877408417ac65194616070f75
Toni Tesori <tt@tesoridesign.com>
2022-11-17 19:23:10 -0500

    Add

--- 2021-11/2021-11.js
+++ 2021-11/2021-11.js
@@ -93,3 +93,8 @@ class Person {
     return `${this.name}s age is ${this.age}`;
   }
 }
+
+//https://www.codewars.com/kata/5a2e9ae2b6cfd7692a0000ba/train/javascript
+function typeOfSum(a, b) {
+  return typeof (a + b);
+}
